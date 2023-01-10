da123418ef2f0fbcb98a1b12249438507e743fba
Elaine Liu <eliu72@uwo.ca>
2023-01-10 12:48:38 -0500

    Create controllers for database CRUD operations

--- app/controllers/location.controller.js
+++ app/controllers/location.controller.js
@@ -5,9 +5,42 @@ const Op = db.Sequelize.Op;
 // Create and Save a new row in Locations table
 exports.create = (req, res) => {
 
+    // Validate request
+    if (!req.body.longitude && !req.body.latitude) {
+        res.status(400).send({
+            message: "Content can not be empty!"
+        });
+        return;
+    }
+    
+    // Create a Location object
+    const location = {
+        longitude: req.body.longitude,
+        latitude: req.body.latitude,
+    };
+
+    // Save in Location db
+    Location.create(location)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+              message:
+                err.message || "Some error occurred while saving the Location."
+            });
+        });
 };
 
 // Retrieve all Locations from the database
 exports.findAll = (req, res) => {
-
+    Location.findAll()
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "Error retrieving all locations"
+            });
+        });
 };
